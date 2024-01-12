05507d5541e018e2813adb187ea3e1763f06c513
e <92055570+ericsstudent@users.noreply.github.com>
2024-01-12 12:18:41 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
 const ItemEntry = {
   title: "",
   url: "",
@@ -26,30 +28,6 @@ const useStorageState = (key, initial) => {
 
   return [value, setValue];
 };
-
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000),
-  );
 
 const storiesReducer = (state, action) => {
   console.log(`state=${action}`);
@@ -86,7 +64,7 @@ const storiesReducer = (state, action) => {
 };
 
 const App = () => {
-  //console.log("App");
+  console.log("App");
 
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
@@ -96,13 +74,18 @@ const App = () => {
   });
 
   React.useEffect(() => {
+    console.log("useEffect");
+
+    if (searchTerm === "") return;
+
     dispatchStories({ type: STORIES_FETCH_INIT });
 
-    getAsyncStories()
+    fetch(API_ENDPOINT + "react")
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: STORIES_FETCH_SUCCESS,
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => {
@@ -124,10 +107,6 @@ const App = () => {
     console.log(event.target.value);
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
 
   //console.log(`isLoading ${stories.isLoading}`);
   //console.log(`isError ${stories.isError}`);
@@ -152,7 +131,7 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      <List list={stories} onRemoveItem={handleRemoveStory} />
 
       <BarChart />
     </div>
