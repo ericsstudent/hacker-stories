a9f9c274a2056648c288097a0d0f98a3e6ef9dbd
e <92055570+ericsstudent@users.noreply.github.com>
2024-01-14 13:53:33 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -83,26 +83,19 @@ const App = () => {
     setUrl(API_ENDPOINT + searchTerm);
   };
 
-  const handleFetchStories = React.useCallback(() => {
+  const handleFetchStories = React.useCallback(async () => {
     console.log("useEffect");
 
     if (!searchTerm) return;
 
     dispatchStories({ type: STORIES_FETCH_INIT });
 
-    axios
-      .get(url)
-      .then((result) => {
-        dispatchStories({
-          type: STORIES_FETCH_SUCCESS,
-          payload: result.data.hits,
-        });
-      })
-      .catch(() => {
-        dispatchStories({
-          type: STORIES_FETCH_FAILURE,
-        });
-      });
+    const result = await axios.get(url);
+
+    dispatchStories({
+      type: STORIES_FETCH_SUCCESS,
+      payload: result.data.hits,
+    });
   }, [url]);
 
   React.useEffect(() => {
