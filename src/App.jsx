cefae0fd7a0b17a28264b84f64dcb40a5153836c
e <92055570+ericsstudent@users.noreply.github.com>
2024-01-14 13:58:11 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -90,12 +90,16 @@ const App = () => {
 
     dispatchStories({ type: STORIES_FETCH_INIT });
 
-    const result = await axios.get(url);
+    try {
+      const result = await axios.get(url);
 
-    dispatchStories({
-      type: STORIES_FETCH_SUCCESS,
-      payload: result.data.hits,
-    });
+      dispatchStories({
+        type: STORIES_FETCH_SUCCESS,
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: STORIES_FETCH_FAILURE });
+    }
   }, [url]);
 
   React.useEffect(() => {
